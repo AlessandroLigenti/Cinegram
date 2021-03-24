6bab1bfde49a84c37e445623ce4bf167732e5e0d
AlessandroLigenti <77884639+AlessandroLigenti@users.noreply.github.com>
2021-03-24 14:09:12 +0100

    Add files via upload

---
+++ Controllers/AreaRiservataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cinegram.Models.Entity;
+using Cinegram.Models.View;
+
+namespace Cinegram.Controllers
+{
+    public class AreaRiservataController : Controller
+    {
+        // GET: AreaRiservata
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var utente = (Utente)Session["UtenteLoggato"];
+            if (utente == null)
+                return RedirectToAction("Login", "Account");
+            var model = new ProfiloViewModel();
+            SetProfiloViewModelLabels(model);
+            model.Utente = utente;
+            return View();
+        }
+
+        private void SetProfiloViewModelLabels(ProfiloViewModel model)
+        {
+            ViewBag.Title = model.LabelTitolo = "Profilo";
+            model.LabelConfermaPassword = "Conferma password";
+            model.LabelEmail = "Indirizzo mail";
+            model.LabelNome = "Nickname";
+            model.LabelPassword = "Password";
+            model.BtnAggiorna = "Aggiorna";
+        }
+
+
+    }
+}
